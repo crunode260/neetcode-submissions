14635d0d20d4c6953ebe273c4990d130817f6fea
crunode260 <134780880+crunode260@users.noreply.github.com>
2026-09-12 11:49:34 +0100

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int climbStairs (int n){
+  if (n==1){
+    return 1;
+  }
+  if (n==2){
+    return 2;
+  }
+  vector<int> stairs(n);
+  stairs[0] = 1;
+  stairs[1] = 2;
+  for (int i = 2; i < n; i++){
+    stairs[i] = stairs[i-2] + stairs[i-1];
+  }
+  return stairs[n-1];
+}};
